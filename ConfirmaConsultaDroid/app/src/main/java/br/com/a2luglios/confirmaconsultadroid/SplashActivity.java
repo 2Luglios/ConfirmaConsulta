0a9498f2427ae653114a2011b8efb21a88ea1dd4
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-08-12 08:33:53 -0300

    mais um com droid

--- ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/SplashActivity.java
+++ ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/SplashActivity.java
@@ -3,26 +3,52 @@ package br.com.a2luglios.confirmaconsultadroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_layout);
+//        setContentView(R.layout.activity_splash_layout);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        setContentView(R.layout.telacomabas);
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+//                finish();
+//            }
+//        }).start();
 
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-                finish();
-            }
-        }).start();
+
+
+
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Tab One");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Tab Two");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("Tab Three");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Tab Three");
+        host.addTab(spec);
     }
 }
