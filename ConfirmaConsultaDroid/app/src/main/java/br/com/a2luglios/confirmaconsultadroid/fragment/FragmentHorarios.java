9cc79cdc357481a856b9ac642fafae42c30388b2
Ettore Luglio <ettore.tamadrum@gmail.com>
2017-08-25 13:04:03 -0300

    agora adicionando eventos nas horas

--- ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/fragment/FragmentHorarios.java
+++ ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/fragment/FragmentHorarios.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
@@ -25,6 +26,8 @@ import br.com.a2luglios.confirmaconsultadroid.R;
 
 public class FragmentHorarios extends Fragment implements WeekView.EventClickListener, WeekView.EventLongPressListener, MonthLoader.MonthChangeListener {
 
+    List<WeekViewEvent> events = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -34,15 +37,35 @@ public class FragmentHorarios extends Fragment implements WeekView.EventClickLis
         // Get a reference for the week view in the layout.
         final WeekView mWeekView = (WeekView) v.findViewById(R.id.weekView);
 
-// Set an action when any event is clicked.
+        // Set an action when any event is clicked.
         mWeekView.setOnEventClickListener(this);
 
-// The week view has infinite scrolling horizontally. We have to provide the events of a
-// month every time the month changes on the week view.
+        // The week view has infinite scrolling horizontally. We have to provide the events of a
+        // month every time the month changes on the week view.
         mWeekView.setMonthChangeListener(this);
 
-// Set long press listener for events.
+        // Set long press listener for events.
         mWeekView.setEventLongPressListener(this);
+
+        mWeekView.setEmptyViewClickListener(new WeekView.EmptyViewClickListener() {
+            @Override
+            public void onEmptyViewClicked(Calendar time) {
+                Toast.makeText(getContext(), "Quer um evento novo? " + time, Toast.LENGTH_LONG).show();
+
+                WeekViewEvent event = new WeekViewEvent();
+                event.setName("Dentista");
+                event.setLocation("Sorriso de Novela");
+                event.setColor(Color.GREEN);
+                event.setStartTime(time);
+                Calendar c = (Calendar) time.clone();
+                c.add(Calendar.HOUR, 1);
+                event.setEndTime(c);
+                events.add(event);
+
+                mWeekView.notifyDatasetChanged();
+
+            }
+        });
 
         mWeekView.setHorizontalFlingEnabled(false);
         mWeekView.setScrollListener(new WeekView.ScrollListener() {
@@ -57,7 +80,7 @@ public class FragmentHorarios extends Fragment implements WeekView.EventClickLis
 
     @Override
     public void onEventClick(WeekViewEvent event, RectF eventRect) {
-
+        Toast.makeText(getActivity(), ""+event.getStartTime(), Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -67,27 +90,6 @@ public class FragmentHorarios extends Fragment implements WeekView.EventClickLis
 
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-        List<WeekViewEvent> events = new ArrayList<>();
-        WeekViewEvent event = new WeekViewEvent();
-        event.setName("Dentista");
-        event.setLocation("Sorriso de Novela");
-        event.setColor(Color.GREEN);
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.HOUR, 1);
-        event.setStartTime(Calendar.getInstance());
-        event.setEndTime(c);
-        events.add(event);
-
-        WeekViewEvent event2 = new WeekViewEvent();
-        event2.setColor(Color.RED);
-        Calendar c1 = Calendar.getInstance();
-        c1.add(Calendar.HOUR, 2);
-        Calendar c2 = Calendar.getInstance();
-        c2.add(Calendar.HOUR, 3);
-        event2.setStartTime(c1);
-        event2.setEndTime(c2);
-        events.add(event2);
-
         return events;
     }
 }
