0a57664cf91ae6f919583f44c3d27d0a2e2fcb0b
Ettore Luglio <ettore.tamadrum@gmail.com>
2017-08-27 21:30:34 -0300

    mais cadastro

--- ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/fragment/FragmentMeusDados.java
+++ ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/fragment/FragmentMeusDados.java
@@ -1,5 +1,7 @@
 package br.com.a2luglios.confirmaconsultadroid.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,23 +14,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 import br.com.a2luglios.confirmaconsultadroid.PrincipalActivity;
 import br.com.a2luglios.confirmaconsultadroid.R;
 import br.com.a2luglios.confirmaconsultadroid.firebase.FirebaseLoginInterface;
+import br.com.a2luglios.confirmaconsultadroid.firebase.FirebaseRTDBUpdate;
 import br.com.a2luglios.confirmaconsultadroid.firebase.FirebaseUtilAuth;
 import br.com.a2luglios.confirmaconsultadroid.firebase.FirebaseUtilDB;
+import br.com.a2luglios.confirmaconsultadroid.modelo.Consultorio;
 import br.com.a2luglios.confirmaconsultadroid.modelo.MapContato;
+import br.com.a2luglios.confirmaconsultadroid.modelo.MapEndereco;
+import br.com.a2luglios.confirmaconsultadroid.modelo.Sexo;
 import br.com.a2luglios.confirmaconsultadroid.modelo.Usuario;
 import br.com.a2luglios.confirmaconsultadroid.util.Preferencias;
 
@@ -42,13 +52,25 @@ public class FragmentMeusDados extends Fragment {
     private EditText campoEmail;
     private EditText campoSenha;
     private EditText campoRepitaSenha;
+    private EditText lembrete;
+    private EditText estadoCivil;
+    private EditText profissao;
+    private EditText nomeMae;
+    private EditText nomePai;
+    private RadioGroup sexo;
+    private DatePicker dataNascimento;
     private EditText campoCPF;
     private EditText campoRG;
     private CheckBox checkSouMedico;
     private EditText campoCRM;
-    private ListView listaEspecialidades;
-    private ListView listaPlanos;
-    private ListView listaConsultorios;
+    private Button btnEspecialidades;
+    private Button btnPlanos;
+    private Button btnConsultorios;
+    private EditText miniCurriculum;
+
+    private List<String> especialistas = new ArrayList<>();
+    private List<String> planos = new ArrayList<>();
+    private List<String> consultorios = new ArrayList<>();
 
     @Nullable
     @Override
@@ -61,59 +83,117 @@ public class FragmentMeusDados extends Fragment {
         campoEmail = (EditText) meusDados.findViewById(R.id.campoEmail);
         campoSenha = (EditText) meusDados.findViewById(R.id.campoSenha);
         campoRepitaSenha = (EditText) meusDados.findViewById(R.id.campoRepitaSenha);
+        lembrete = (EditText) meusDados.findViewById(R.id.campoLembrete);
+        estadoCivil = (EditText) meusDados.findViewById(R.id.campoEstadoCivil);
+        profissao = (EditText) meusDados.findViewById(R.id.campoProfissao);
+        nomeMae = (EditText) meusDados.findViewById(R.id.campoNomeMae);
+        nomePai = (EditText) meusDados.findViewById(R.id.campoNomePai);
+        sexo = (RadioGroup) meusDados.findViewById(R.id.radioSexo);
+        dataNascimento = (DatePicker) meusDados.findViewById(R.id.dateNascimento);
         campoCPF = (EditText) meusDados.findViewById(R.id.campoCPF);
         campoRG = (EditText) meusDados.findViewById(R.id.campoRG);
         checkSouMedico = (CheckBox) meusDados.findViewById(R.id.checkSouMedico);
         campoCRM = (EditText) meusDados.findViewById(R.id.campoCRM);
+        btnEspecialidades = (Button) meusDados.findViewById(R.id.btnEspecialidades);
+        btnPlanos = (Button) meusDados.findViewById(R.id.btnPlanos);
+        btnConsultorios = (Button) meusDados.findViewById(R.id.btnConsultorios);
+        miniCurriculum = (EditText) meusDados.findViewById(R.id.campoMiniCurriculum);
 
-        listaEspecialidades = (ListView) meusDados.findViewById(R.id.listaEspecialidades);
-        listaEspecialidades.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        listaEspecialidades.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_multiple_choice, Arrays.asList("la", "lala", "lalala")));
+        btnEspecialidades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(especialistas);
+            }
+        });
 
-        listaPlanos = (ListView) meusDados.findViewById(R.id.listaPlanos);
-        listaPlanos.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        listaPlanos.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_multiple_choice, Arrays.asList("la", "lala", "lalala")));
+        btnPlanos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(planos);
+            }
+        });
 
-        listaConsultorios = (ListView) meusDados.findViewById(R.id.listaConsultorios);
-        listaConsultorios.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        listaConsultorios.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_multiple_choice, Arrays.asList("la", "lala", "lalala")));
+        btnConsultorios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(consultorios);
+            }
+        });
 
         Usuario usuario = new Preferencias(getContext()).getUsuario();
         if ( usuario.getToken().equals("") ) {
-            campoNome.setText(usuario.getNome());
-
             MapContato contato = new MapContato();
             contato.setMapa(usuario.getContato());
 
+            MapEndereco endereco = new MapEndereco();
+            endereco.setMapa(usuario.getEndereco());
+
+            Calendar nascimento = Calendar.getInstance();
+            nascimento.setTimeInMillis(usuario.getNascimento());
+
+            campoNome.setText(usuario.getNome());
             campoEmail.setText(contato.getEmail());
+            lembrete.setText(usuario.getLembrete());
+            estadoCivil.setText(usuario.getEstadoCivil());
+            profissao.setText(usuario.getProfissao());
+            nomeMae.setText(usuario.getNomeMae());
+            nomePai.setText(usuario.getNomePai());
+            if ( usuario.getSexo() == Sexo.Masculino ) sexo.check(R.id.sexoMasculino); else sexo.check(R.id.sexoFeminino);
+            dataNascimento.init(nascimento.get(Calendar.YEAR), nascimento.get(Calendar.MONTH), nascimento.get(Calendar.DAY_OF_MONTH), null);
             campoCPF.setText(usuario.getCpf());
             campoRG.setText(usuario.getRg());
             checkSouMedico.setChecked(usuario.isEhMedico());
             campoCRM.setText(usuario.getCRM());
+            miniCurriculum.setText(usuario.getMiniCurriculum());
 
-            if ( checkSouMedico.isChecked() ) {
-                campoCRM.setEnabled(true);
-            } else {
-                campoCRM.setEnabled(false);
-            }
+            mudaEstado(checkSouMedico.isChecked());
         }
 
         checkSouMedico.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if ( b ) {
-                    campoCRM.setEnabled(true);
-                    Toast.makeText(getContext(), "Sou Médico", Toast.LENGTH_LONG).show();
-                } else {
-                    campoCRM.setEnabled(false);
-                    Toast.makeText(getContext(), "Não Sou Médico", Toast.LENGTH_LONG).show();
-                }
+                mudaEstado(b);
             }
         });
 
         setHasOptionsMenu(true);
 
         return meusDados;
+    }
+
+    private void mudaEstado(boolean estado) {
+        campoCRM.setEnabled(estado);
+        btnEspecialidades.setEnabled(estado);
+        btnPlanos.setEnabled(estado);
+        btnConsultorios.setEnabled(estado);
+        miniCurriculum.setEnabled(estado);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        new FirebaseUtilDB().readRTDBPlain("especialidades", new FirebaseRTDBUpdate() {
+            @Override
+            public void updateMensagem(Object obj) {
+                especialistas.add((String)obj);
+            }
+        });
+
+        new FirebaseUtilDB().readRTDBPlain("planos", new FirebaseRTDBUpdate() {
+            @Override
+            public void updateMensagem(Object obj) {
+                planos.add((String)obj);
+            }
+        });
+
+        new FirebaseUtilDB().readRTDB("consultorios", Consultorio.class,new FirebaseRTDBUpdate() {
+            @Override
+            public void updateMensagem(Object obj) {
+                final Consultorio consultorio = (Consultorio) obj;
+                consultorios.add(consultorio.getNome());
+            }
+        });
     }
 
     @Override
@@ -137,13 +217,25 @@ public class FragmentMeusDados extends Fragment {
                                 public void toDo(FirebaseUser user) {
                                     Usuario usuario = new Usuario();
                                     usuario.setNome(campoNome.getEditableText().toString());
+
                                     MapContato contato = new MapContato();
                                     contato.setEmail(campoEmail.getEditableText().toString());
                                     usuario.setContato(contato.getMapa());
+
+                                    MapEndereco endereco = new MapEndereco();
+                                    endereco.setCep("");
+                                    usuario.setEndereco(endereco.getMapa());
+
                                     usuario.setCpf(campoCPF.getEditableText().toString());
                                     usuario.setRg(campoRG.getEditableText().toString());
                                     usuario.setEhMedico(checkSouMedico.isChecked());
                                     usuario.setCRM(campoCRM.getEditableText().toString());
+
+                                    usuario.setEspecialidades(new ArrayList<String>());
+                                    usuario.getEspecialidades().add("lalalalala");
+
+                                    usuario.setPlanos(new ArrayList<String>());
+                                    usuario.getPlanos().add("lalalalala");
 
                                     usuario.setConsultorios(new ArrayList<String>());
                                     usuario.getConsultorios().add("lalalalala");
@@ -174,33 +266,36 @@ public class FragmentMeusDados extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
-    public void pegaClicado(View v) {
-//        SparseBooleanArray checked = listView.getCheckedItemPositions();
-//        ArrayList<String> selectedItems = new ArrayList<String>();
-//        for (int i = 0; i < checked.size(); i++) {
-//            // Item position in adapter
-//            int position = checked.keyAt(i);
-//            // Add sport if it is checked i.e.) == TRUE!
-//            if (checked.valueAt(i))
-//                selectedItems.add(adapter.getItem(position));
-//        }
-//
-//        String[] outputStrArr = new String[selectedItems.size()];
-//
-//        for (int i = 0; i < selectedItems.size(); i++) {
-//            outputStrArr[i] = selectedItems.get(i);
-//        }
-//
-//        Intent intent = new Intent(getContext(),ResultActivity.class);
-//
-//        // Create a bundle object
-//        Bundle b = new Bundle();
-//        b.putStringArray("selectedItems", outputStrArr);
-//
-//        // Add the bundle to the intent.
-//        intent.putExtras(b);
-//
-//        // start the ResultActivity
-//        startActivity(intent);
+    private void showDialog(List<String> listToShow) {
+        // TODO tem que trazer populado com o que o cara já escreveu...
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(getContext());
+        dialog.setTitle("Lista");
+        View v = LayoutInflater.from(getContext()).inflate(R.layout.dialog_generic_list, null);
+        final ListView lista = (ListView) v.findViewById(R.id.lista);
+        lista.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        lista.setAdapter(new ArrayAdapter<String>(getContext(),
+                        android.R.layout.simple_list_item_multiple_choice, listToShow));
+        dialog.setView(v);
+        dialog.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int a) {
+                SparseBooleanArray checked = lista.getCheckedItemPositions();
+                ArrayList<String> selectedItems = new ArrayList<String>();
+                for (int i = 0; i < checked.size(); i++) {
+                    int position = checked.keyAt(i);
+                    if (checked.valueAt(i)) selectedItems.add((String) lista.getAdapter().getItem(position));
+                }
+
+                String[] outputStrArr = new String[selectedItems.size()];
+
+                for (int i = 0; i < selectedItems.size(); i++) {
+                    outputStrArr[i] = selectedItems.get(i);
+                }
+                Toast.makeText(getContext(), "Selecionou: " + selectedItems.size() + " itens", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.show();
     }
+
 }
--- ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/util/Preferencias.java
+++ ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/util/Preferencias.java
@@ -3,9 +3,15 @@ package br.com.a2luglios.confirmaconsultadroid.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.prefs.Preferences;
 
 import br.com.a2luglios.confirmaconsultadroid.modelo.MapContato;
+import br.com.a2luglios.confirmaconsultadroid.modelo.MapEndereco;
+import br.com.a2luglios.confirmaconsultadroid.modelo.Sexo;
 import br.com.a2luglios.confirmaconsultadroid.modelo.Usuario;
 
 /**
@@ -25,17 +31,48 @@ public class Preferencias {
     public void setUsuario (Usuario usuario) {
         SharedPreferences.Editor edit = ctx.getSharedPreferences(USUARIO, Context.MODE_PRIVATE).edit();
         //edit.putLong("id", usuario.getId());
+        edit.putString("hash", usuario.getHash());
         edit.putString("nome", usuario.getNome());
+        edit.putString("lembrete", usuario.getLembrete());
+        edit.putBoolean("ehMedico", usuario.isEhMedico());
+        edit.putLong("nascimento", usuario.getNascimento());
+        edit.putString("sexo", usuario.getSexo().name());
+        edit.putString("estadoCivil", usuario.getEstadoCivil());
+        edit.putString("profissao", usuario.getProfissao());
+        edit.putString("nomeMae", usuario.getNomeMae());
+        edit.putString("nomePai", usuario.getNomePai());
+        edit.putString("token", usuario.getToken());
+        edit.putString("cpf", usuario.getCpf());
+        edit.putString("rg", usuario.getRg());
 
         MapContato contato = new MapContato();
         contato.setMapa(usuario.getContato());
         edit.putString("email", contato.getEmail());
+        edit.putString("celular", contato.getCelular());
+        edit.putString("telefoneComercial", contato.getTelefoneComercial());
+        edit.putString("telefoneResidencial", contato.getTelefoneResidencial());
+        edit.putString("contatoEmergencia", contato.getContatoEmergencia());
+        edit.putString("telefoneEmergencia", contato.getTelefoneEmergencia());
 
-        edit.putString("cpf", usuario.getCpf());
-        edit.putString("rg", usuario.getRg());
-        edit.putBoolean("souMedico", usuario.isEhMedico());
+        MapEndereco endereco = new MapEndereco();
+        endereco.setMapa(usuario.getEndereco());
+        edit.putString("cep", endereco.getCep());
+        edit.putString("bairro", endereco.getBairro());
+        edit.putString("cidade", endereco.getCidade());
+        edit.putString("complemento", endereco.getComplemento());
+        edit.putString("estado", endereco.getEstado());
+        edit.putString("logradouro", endereco.getLogradouro());
+        edit.putString("numero", endereco.getNumero());
+        edit.putString("pais", endereco.getPais());
+        edit.putString("latitude", Double.toString(endereco.getLatitude()));
+        edit.putString("longitude", Double.toString(endereco.getLongitude()));
+
         edit.putString("crm", usuario.getCRM());
-        edit.putString("token", usuario.getToken());
+        edit.putStringSet("especialidades", converteListStringToSetString(usuario.getEspecialidades()));
+        edit.putStringSet("planos", converteListStringToSetString(usuario.getPlanos()));
+        edit.putStringSet("consultorios", converteListStringToSetString(usuario.getConsultorios()));
+        edit.putString("miniCurriculum", usuario.getMiniCurriculum());
+
         edit.commit();
     }
 
@@ -43,18 +80,66 @@ public class Preferencias {
         Usuario usuario = new Usuario () ;
         SharedPreferences prefs = ctx.getSharedPreferences(USUARIO, Context.MODE_PRIVATE);
        //usuario.setId(prefs.getLong("id", -1));
-        usuario.setNome(prefs.getString("nome", ""));
+        usuario.setHash(prefs.getString("hash", null));
+        usuario.setNome(prefs.getString("nome", null));
+        usuario.setLembrete(prefs.getString("lembrete", ""));
+        usuario.setEhMedico(prefs.getBoolean("ehMedico", false));
+        usuario.setNascimento(prefs.getLong("nascimento", 0));
+        usuario.setSexo(Sexo.valueOf(prefs.getString("sexo", Sexo.Masculino.name())));
+        usuario.setEstadoCivil(prefs.getString("estadoCivil", ""));
+        usuario.setProfissao(prefs.getString("profissao", ""));
+        usuario.setNomeMae(prefs.getString("nomeMae", ""));
+        usuario.setNomePai(prefs.getString("nomePai", ""));
+        usuario.setToken(prefs.getString("token", ""));
+        usuario.setCpf(prefs.getString("cpf", ""));
+        usuario.setRg(prefs.getString("rg", ""));
 
         MapContato contato = new MapContato();
         contato.setEmail(prefs.getString("email", ""));
+        contato.setCelular(prefs.getString("celular", ""));
+        contato.setTelefoneComercial(prefs.getString("telefoneComercial", ""));
+        contato.setTelefoneResidencial(prefs.getString("telefoneResidencial", ""));
+        contato.setContatoEmergencia(prefs.getString("contatoEmergencia", ""));
+        contato.setTelefoneEmergencia(prefs.getString("telefoneEmergencia", ""));
         usuario.setContato(contato.getMapa());
 
-        usuario.setCpf(prefs.getString("cpf", ""));
-        usuario.setRg(prefs.getString("rg", ""));
-        usuario.setEhMedico(prefs.getBoolean("souMedico", false));
+        MapEndereco endereco = new MapEndereco();
+        endereco.setCep(prefs.getString("cep", ""));
+        endereco.setBairro(prefs.getString("bairro", ""));
+        endereco.setCidade(prefs.getString("cidade", ""));
+        endereco.setComplemento(prefs.getString("complemento", ""));
+        endereco.setEstado(prefs.getString("estado", ""));
+        endereco.setLatitude(Double.parseDouble(prefs.getString("latitude", "0")));
+        endereco.setLongitude(Double.parseDouble(prefs.getString("longitude", "0")));
+        endereco.setLogradouro(prefs.getString("logradouro", ""));
+        endereco.setNumero(prefs.getString("numero", ""));
+        endereco.setPais(prefs.getString("pais", ""));
+        usuario.setEndereco(endereco.getMapa());
+
+        Set<String> vazio = new HashSet<>();
+
         usuario.setCRM(prefs.getString("crm", ""));
-        usuario.setToken(prefs.getString("token", ""));
+        usuario.setEspecialidades(converteSetStringToListString(prefs.getStringSet("", vazio)));
+        usuario.setPlanos(converteSetStringToListString(prefs.getStringSet("", vazio)));
+        usuario.setConsultorios(converteSetStringToListString(prefs.getStringSet("", vazio)));
+        usuario.setCRM(prefs.getString("miniCurriculum", ""));
 
         return usuario;
+    }
+
+    public Set<String> converteListStringToSetString(List<String> lista) {
+        Set<String> set = new HashSet<>();
+        for(String s : lista) {
+            set.add(s);
+        }
+        return set;
+    }
+
+    public List<String> converteSetStringToListString(Set<String> set) {
+        List<String> lista = new ArrayList<>();
+        for(String s : set) {
+            lista.add(s);
+        }
+        return lista;
     }
 }
