6f76a22dc3697f35e52bd4fbc42c98d5b02e3da1
Ettore Luglio <ettore.tamadrum@gmail.com>
2017-08-28 21:10:05 -0300

    aba de mensagens

--- ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/fragment/FragmentNotificacoes.java
+++ ConfirmaConsultaDroid/app/src/main/java/br/com/a2luglios/confirmaconsultadroid/fragment/FragmentNotificacoes.java
@@ -1,13 +1,17 @@
 package br.com.a2luglios.confirmaconsultadroid.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.Arrays;
 
 import br.com.a2luglios.confirmaconsultadroid.R;
 
@@ -19,13 +23,28 @@ public class FragmentNotificacoes extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
-        View agenda =
-                inflater.inflate(R.layout.fragment_notificacoes,
+        View agenda = inflater.inflate(R.layout.fragment_notificacoes,
                         container, false);
 
         ListView listNotificacoes = (ListView) agenda.findViewById(R.id.listNotificacoes);
+        listNotificacoes.setAdapter(new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_list_item_1, Arrays.asList("", "", "", "", "", "")) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View v1 = LayoutInflater.from(getContext()).inflate(R.layout.item_mensagem_layout, null);
+                View v2 = LayoutInflater.from(getContext()).inflate(R.layout.item_propaganda_layout, null);
+
+                if ( position % 2 == 1 ) {
+                    return v1;
+                } else {
+                    return v2;
+                }
+            }
+        });
 
         listNotificacoes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
